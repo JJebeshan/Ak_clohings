7af877642e8224b61d23dde63b5f6a28bf158b8c
JJebeshan <jebeshanpro@gmail.com>
2025-02-06 18:33:22 +0530

    seprated files updated

--- AK/AKC/views.py
+++ AK/AKC/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render,redirect,HttpResponse
+from django.templatetags.static import static
 # Create your views here.
 
 def home(request):
-    return render(request,'index.html')
+    slides = [
+        static("banner1.png"),
+        static("banner2.png"),
+        static("banner3.jpg"),
+    ]
+    return render(request, "index.html", {"slides": slides})
