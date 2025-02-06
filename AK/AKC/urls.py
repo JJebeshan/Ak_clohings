6dae220f1261d54adc7ff0b51956b201121d4f0b
JJebeshan <jebeshanpro@gmail.com>
2025-02-06 23:58:18 +0530

    profile settings updates login and signup

--- AK/AKC/urls.py
+++ AK/AKC/urls.py
@@ -5,5 +5,12 @@ from . import views
 urlpatterns=[
    path('',views.home, name='home'),
    path('profile',views.profile,name='profile'),
-   path('orders',views.orders, name='orders')
+   path('orders',views.orders, name='orders'),
+   path('coupons',views.coupons,name='coupons'),
+   path('signup',views.signup,name='signup'),
+   path('address_list',views.address_list,name='address_list'),
+   path('login',views.login,name='login'),
+   path('otp',views.otp,name='otp'),
+   path('password',views.pass_word,name='password'),
+   path('changepass',views.password_change,name='changepas')
 ]
--- AK/AKC/views.py
+++ AK/AKC/views.py
@@ -15,3 +15,22 @@ def profile(request):
 
 def orders(request):
     return render(request,'orders.html')
+
+def coupons(request):
+    return render(request,'coupons.html')
+
+def signup(request):
+    return render(request,'signup.html')
+
+def address_list(request):
+    return render(request,'address_list.html')
+def login(request):
+    return render(request,'login.html')
+
+def otp(request):
+    return render(request,'otp.html')
+
+def pass_word(request):
+    return render(request,'password.html')
+def password_change(request):
+    return render(request,'change_pass.html')
